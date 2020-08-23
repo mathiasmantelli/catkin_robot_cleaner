54800aa4d75c2c6e6bb11f5758898c4209a75a00
Mathias Mantelli <mathiasfassini@gmail.com>
2020-08-23 13:00:42 -0300

    First commit.

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/mathias/catkin_robot_cleaner/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/mathias/catkin_robot_cleaner/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/mathias/catkin_robot_cleaner/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/mathias/catkin_robot_cleaner/src:$ROS_PACKAGE_PATH"
