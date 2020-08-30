3e9260a4951cd0b7d54e72858c5305089044a4fb
Mathias Mantelli <mathiasfassini@gmail.com>
2020-08-30 20:20:49 -0300

    The gridcleaner function in Python is working.

--- src/turtlesim_cleaner/src/robot_cleaner.py
+++ src/turtlesim_cleaner/src/robot_cleaner.py
@@ -60,9 +60,9 @@ def rotate(angular_speed, relative_angle, isClockwise):
     velocity_message.angular.x = 0
     velocity_message.angular.y = 0
     if (isClockwise):
-        velocity_message.angular.z = abs(angular_speed)
-    else:
         velocity_message.angular.z = -abs(angular_speed)
+    else:
+        velocity_message.angular.z = abs(angular_speed)
 
     time_t0 = rospy.Time.now().to_sec()
     current_angle = 0
@@ -79,6 +79,55 @@ def rotate(angular_speed, relative_angle, isClockwise):
     velocity_message.angular.z = 0
     velocity_publisher.publish(velocity_message)
 
+def setDesideredOrientation(desired_angle):
+    difference_angle = desired_angle - theta
+    isClockwise_h = False
+    if (difference_angle < 0):
+        isClockwise_h = True
+    rotate(abs(difference_angle), abs(difference_angle), isClockwise_h)
+
+def moveGoal(goal_pose, distance_tolerance):
+    global x, y, theta
+    vel_msg = Twist()
+    test = True
+    while (test):
+        K_linear = 0.5
+        distance = abs(math.sqrt(((goal_pose.x-x)**2) + ((goal_pose.y - y)**2)))
+        linear_speed = distance * K_linear
+
+        K_angular = 4.0
+        desired_angle_goal = math.atan2(goal_pose.y-y, goal_pose.x-x)
+        angular_speed = (desired_angle_goal-theta)*K_angular
+
+        vel_msg.linear.x = linear_speed
+        vel_msg.angular.z = angular_speed
+
+        velocity_publisher.publish(vel_msg)
+
+        if (distance < distance_tolerance):
+            setDesideredOrientation(degrees2radians(goal_pose.theta))
+            test = False
+
+def gridClean():
+    desired_pose = Pose()
+    desired_pose.x = 1
+    desired_pose.y = 1
+    desired_pose.theta = 0
+
+    moveGoal(desired_pose, 0.01)
+
+    move(2.0, 9.0, True)
+    rotate(degrees2radians(20), degrees2radians(90), False)
+    move(2.0, 9.0, True)
+    rotate(degrees2radians(20), degrees2radians(90), False)
+    move(2.0, 1.0, True)
+    rotate(degrees2radians(20), degrees2radians(90), False)
+    move(2.0, 9.0, True)
+    rotate(degrees2radians(30), degrees2radians(90), True)
+    move(2.0, 1.0, True)
+    rotate(degrees2radians(30), degrees2radians(90), True)
+    move(2.0, 9.0, True)
+    pass
 
 if __name__ == '__main__':
     try:
@@ -87,7 +136,18 @@ if __name__ == '__main__':
         pose_subscriber = rospy.Subscriber("/turtle1/pose", Pose, poseCallBack) 
         time.sleep(2)
         #move(2, 2, 1)
-        rotate(degrees2radians(30), degrees2radians(90), 1)
+        
+        #rotate(degrees2radians(30), degrees2radians(90), 1)
+        
+        #setDesideredOrientation(degrees2radians(120))
+        
+        #goal_pose = Pose()
+        #goal_pose.x = 2
+        #goal_pose.y = 1
+        #goal_pose.theta = degrees2radians(90)
+        #moveGoal(goal_pose, 0.01);
+
+        gridClean();
         rospy.spin()
 
     except rospy.ROSInternalException:
